b17ea346f26e7efd982ac6c541307d52ea1eb88c
genesis <haritonovgleb123@gmail.com>
2022-01-08 05:08:00 +0300

    moved logic to parser class

--- url_parser/parser.py
+++ url_parser/parser.py
@@ -1,38 +1,160 @@
+import shutil
+import tempfile
 import requests
 from bs4 import BeautifulSoup, Comment
-from urllib.parse import urljoin, urlparse
+# from jinja2 import Environment, PackageLoader, select_autoescape
+import jinja2
+import requests
+import os
+import time
+import urllib.parse
+
+from werkzeug.wrappers import response
 
 # html tags that do not contain valueable text
-not_parse = [
-    "button",
-    "style",
-    "script",
-    "svg",
-    "form",
-    "nav",
-    "header",
-    "footer",
-    "menu",
-]
-not_parse_class = [".menu", ".banner"]
 
 
-def clean_soup(soup):
+def strip_scheme(url):
+    parsed = urllib.parse.urlparse(url).netloc + urllib.parse.urlparse(url).path
+    return parsed.replace("/", "_").rstrip("/")
 
-    # remove comments:
-    for comments in soup.find_all(text=lambda text: isinstance(text, Comment)):
-        comments.extract()
 
-    # remove unnecessary tags from soup
-    for tag in not_parse:
-        for el in soup.find_all(tag):
-            el.extract()
+class Parser:
+    # 
+    def __init__(self, templates_path="templates", caching: int=1, caching_duration: int=12*60*60, cache_folder: str="cache", mode: str='web-service'):
+        self.templates_path = templates_path
+        self.template = self.init_template_env()
 
-    for cl in not_parse_class:
-        for el in soup.select(cl):
-            el.extract()
+        self.caching = caching
+        self.caching_duration = caching_duration
+        self.cache_folder = cache_folder
 
-    # remove head
-    soup.find("head").extract()
+        self.not_parse = [
+            "button",
+            "style",
+            "script",
+            "svg",
+            "form",
+            "nav",
+            "header",
+            "footer",
+            "menu",
+        ]
+        self.not_parse_class = [".menu", ".banner"]
 
-    return soup
+    @staticmethod
+    def get_soup(response):
+        return BeautifulSoup(response.content, 'html.parser')
+
+    def clean_soup(self, soup):
+        for comments in soup.find_all(text=lambda text: isinstance(text, Comment)):
+            comments.extract()
+
+        # remove unnecessary tags from soup
+        for tag in self.not_parse:
+            for el in soup.find_all(tag):
+                el.extract()
+
+        for cl in self.not_parse_class:
+            for el in soup.select(cl):
+                el.extract()
+
+        # remove head
+        soup.find("head").extract()
+
+        return soup
+
+    def init_template_env(self):
+        templateLoader = jinja2.FileSystemLoader(
+            searchpath=os.path.join(os.getcwd(), self.templates_path))
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        TEMPLATE_FILE = "response.txt.jinja"
+        template = templateEnv.get_template(TEMPLATE_FILE)
+        return template
+
+    def render_template(self, **kwargs):
+        return self.template.render(**kwargs)
+
+    @staticmethod
+    def get_and_check_url_response(url):
+        if not urllib.parse.urlparse(url).scheme:
+            url = "https://" + url
+        
+        try:
+            response = requests.get(url)
+        except requests.exceptions.ConnectionError:
+            raise Exception(400, "Connection to this url can't be esatblished")
+
+        st_code = response.status_code
+        if not st_code == requests.codes.ok:
+            raise Exception(st_code, "Request return {} status code".format(str(st_code)))
+
+        return response
+
+    @staticmethod
+    def generate_file_name(url: str, width: int, img_flag: int) -> str:
+        return strip_scheme(url) + "_w-" + str(width) + "_i-" + str(img_flag) + ".txt"
+
+    def generate_cache_path(self, url, width, img_flag):
+        file_name = self.generate_file_name(url, width, img_flag)
+        cached_file_path = os.path.abspath(os.path.join(self.cache_folder, file_name))
+        return cached_file_path
+
+    def file_exists_in_cache(self, cached_file_path: str) -> bool:
+        return os.path.isfile(cached_file_path) and (time.time() - os.path.getmtime(cached_file_path) < self.caching_duration)
+
+    def generate_txt_file(self, contents: str) -> tempfile.NamedTemporaryFile:
+        tmp = tempfile.NamedTemporaryFile()
+        tmp.write(contents.encode('utf-8'))
+        tmp.seek(0, 2)
+        return tmp
+
+    def save_to_cache(self, url, width, img_flag, tmp: tempfile.NamedTemporaryFile):
+        file_name = self.generate_file_name(url, width, img_flag)
+        cached_file_path = os.path.abspath(os.path.join(self.cache_folder, file_name))
+        shutil.copyfile(tmp.name, cached_file_path)
+        tmp.close()
+
+
+def main():
+    import argparse
+    arg_parser = argparse.ArgumentParser(description='Parse URL to txt.')
+    arg_parser.add_argument('--url', type=str, help='URL to parse.')
+    arg_parser.add_argument('--width', type=int, default=200, help='Width of the output.')
+    arg_parser.add_argument('--img', type=int, default=1, help='Include images in the output.')
+    arg_parser.add_argument('--cache', type=int, default=1, help="Cache results.")
+    args = arg_parser.parse_args()
+    if not args.url:
+        print("You must specify url with --url")
+        return
+
+    url = args.url
+    width = args.width
+    img_flag = args.img
+
+    parser = Parser(caching=args.cache)
+    if parser.caching:
+        cached_file_path = parser.generate_cache_path(url, width, img_flag)
+        if parser.file_exists_in_cache(cached_file_path):
+            with open(cached_file_path) as f:
+                print(f.read())
+                return
+    try:
+        response = parser.get_and_check_url_response(url)
+    except Exception as e:
+        print("{}\n{}".format(e.args[0], e.args[1]))
+        return
+
+    soup = parser.get_soup(response)
+    cleaned_soup = parser.clean_soup(soup)
+    rendered_template = parser.render_template(
+        soup=cleaned_soup, 
+        width=width,
+        img_flag=img_flag
+    )
+
+    print(rendered_template)
+
+
+if __name__ == "__main__":
+    main()
--- url_parser/url_parser.py
+++ url_parser/url_parser.py
@@ -1,29 +1,21 @@
-from urllib import parse
-from bs4 import BeautifulSoup
-
-from flask import Blueprint, render_template, request, current_app
+from flask import Blueprint, request
 from flask import send_file, after_this_request, request, abort
 
-from .parser import clean_soup
-import tempfile
-import os, shutil, time
-import requests
-import urllib.parse
+# from .parser import clean_soup
+from .parser import Parser, strip_scheme
+import shutil
+from .config import *
 
 bp = Blueprint("url_parser", __name__, url_prefix="/")
 
-
-def strip_scheme(url):
-    parsed = parse.urlparse(url).netloc + parse.urlparse(url).path
-    return parsed.replace("/", "_")
-
+parser = Parser(
+    templates_path="url_parser/templates",
+    caching=CACHE,
+    caching_duration=CACHE_DURATION,
+    cache_folder=CACHE_FOLDER)
 
 @bp.route("/", methods=["GET"])
 def parse_url_page():
-    caching = current_app.config["CACHE"]
-    caching_duration = current_app.config["CACHE_DURATION"]
-    cache_folder = current_app.config["CACHE_FOLDER"]
-
     # Try to close temp file after request.
     @after_this_request
     def clean_up(response):
@@ -39,65 +31,35 @@ def parse_url_page():
 
     url = request.args.get("url")
 
-    if not urllib.parse.urlparse(url).scheme:
-        url = "https://" + url
-
-    # Check if url returns ok code.
     try:
-        r = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        abort(404, "URL not found.")
+        response = parser.get_and_check_url_response(url)
+    except Exception as e:
+        abort(e[0], e[1])
 
-    st_code = r.status_code
-    if not st_code == requests.codes.ok:
-        abort(st_code, "Url returned: {} status code".format(str(st_code)))
+    width = int(request.args.get('width') or 200)
+    img_flag = int(request.args.get('img') or 1)
 
-    # Width parameter.
-    if request.args.get("width"):
-        width = int(request.args.get("width"))
-    else:
-        # Defalut value
-        width = 200
+    file_name = parser.generate_file_name(url, width, img_flag)
 
-    # Img_flag (parse images or not).
-    if request.args.get("img"):
-        img_flag = 1 if int(request.args.get("img")) else 0
-    else:
-        # Default value
-        img_flag = 1
+    if parser.caching:
+        cached_file_path = parser.generate_cache_path(url, width, img_flag)
+        if parser.file_exists_in_cache(cached_file_path):
+            return send_file(
+                cached_file_path, as_attachment=True, attachment_filename=file_name
+            )
 
-    # Generate file name for download.
-    file_name = strip_scheme(url) + "_w-" + str(width) + "_i-" + str(img_flag) + ".txt"
-
-    if caching:
-        cached_file_path = os.path.abspath(os.path.join(cache_folder, file_name))
-
-    if (
-        caching
-        and os.path.isfile(cached_file_path)
-        and (time.time() - os.path.getmtime(cached_file_path) < caching_duration)
-    ):
-        # If caching enabled and cached file exists and was created less than caching duration ago.
-        # Return cached file.
-        return send_file(
-            cached_file_path, as_attachment=True, attachment_filename=file_name
-        )
-
-    tree = BeautifulSoup(r.content, "html.parser")
-    tree = clean_soup(tree)
-
-    # Generate txt file.
-    rendered_template = render_template(
-        "response.txt.jinja", tree=tree, width=width, img_flag=img_flag
+    soup = parser.get_soup(response)
+    cleaned_soup = parser.clean_soup(soup)
+    rendered_template = parser.render_template(
+        soup=cleaned_soup, 
+        width=width,
+        img_flag=img_flag
     )
 
-    # Create temporary file and write generated txt to it.
-    tmp = tempfile.NamedTemporaryFile()
-    tmp.write(rendered_template.encode("utf-8"))
-    tmp.seek(0, 2)
+    tmp = parser.generate_txt_file(rendered_template)
 
     # Save tempfile on disk if caching enabled.
-    if caching:
+    if parser.caching:
         shutil.copyfile(tmp.name, cached_file_path)
 
     # Send file to the user.
